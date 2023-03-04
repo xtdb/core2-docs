1915ce941ebf1570bc99793cf734fbe303cb0afc
James Henderson <james@jarohen.dev>
2023-03-04 20:30:50 +0000

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), tailwind()],
+  trailingSlash: 'never'
+});
