cf90b11fc6077150509915365ff93d288d2e988c
James Henderson <james@jarohen.dev>
2023-03-11 23:17:47 +0000

    contact, support, terms

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['Atkinson Hyperlegible', ...defaultTheme.fontFamily.mono],
+            mono: ['PT Mono', ...defaultTheme.fontFamily.mono]
+		},
 	},
 	plugins: [],
 }
