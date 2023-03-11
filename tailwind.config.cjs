4f3ea00f726368f9f5b69c4d678ff9d2794dc8f3
James Henderson <james@jarohen.dev>
2023-03-11 16:27:23 +0000

    home page content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,css}'],
 	theme: {
 		extend: {},
 	},
